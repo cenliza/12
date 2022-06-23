d0de6f256d11fd40a64d13b75766f1f529cdaf46
cenliza <105453075+cenliza@users.noreply.github.com>
2022-06-23 22:32:45 +0300

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,29 @@
+class train:
+    def __init__(self, wheels, speed, wagons ,motor):
+    self.wheels = wheels
+    self.skorost = skorost
+    self.speed = speed
+    self.driver's cab = driver's cab
+    self.wagons = wagons
+    self.motor = motor
+
+    class wheels:
+        def __init__(self, traction, wagon):
+        self.traction = traction
+        self.wagon = wagon
+
+    class speed:
+        speed("140км/ч"if"160км/ч")
+
+    class wagon:
+        def __init__(self, seated wagon, Econom-class train, Compartment carriage, SV wagon):
+            self.wheels = wheels
+
+        self.seated wagon = seated wagon
+        self.Econom-class train = Econom-class train
+        self.Compartment carriag = Compartment carriag
+        self.SV wagon = SV wagon
+
+    class motor:
+        def __init__(self,speed) :
+            speed("50км/ч")
